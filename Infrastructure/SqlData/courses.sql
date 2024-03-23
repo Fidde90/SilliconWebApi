e1ab5f93694392604757ac8df023b799016c6c3a
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-03-23 23:42:11 +0100

    Get one course is working

---
+++ Infrastructure/SqlData/courses.sql
@@ -0,0 +1,9 @@
+insert into Courses values('Fullstack web developer course from scratch','Robert Fox','12.50','https://localhost:7198\images\course-laptop.svg','true','4.2','94','','220');
+insert into Courses values('HTML, CSS, JavaScript Web Developer','Jenny Wilson & Marvin McKinney','15.99','https://localhost:7198\images\course-html-css-javascript.svg','false','3.1','92','','140');
+insert into Courses values('The Complete Front-End Web Development Cours','Albert Floresx','44.99','https://localhost:7198\images\course-front-end.svg','false','2.7','98','9.99','100');
+insert into Courses values('iOS & Swift - The Complete iOS App Development Course','Marvin McKinney','15.99','https://localhost:7198\images\course-ios-swift.svg','false','3.1','92','','160');
+insert into Courses values('Data Science & Machine Learning with Python','Esther Howard','11.20','https://localhost:7198\images\course-data-science.svg','true','3.1','92','','160');
+insert into Courses values('Creative CSS Drawing Course: Make Art With CSS','Robert Fox','10.50','https://localhost:7198\images\course-css-drawing.svg','false','4.2','94','','220');
+insert into Courses values('Blender Character Creator v2.0 for Video Games Design','Ralph Edwards','8.99','https://localhost:7198\images\course-blender.svg','false','3.1','92','','160');
+insert into Courses values('The Ultimate Guide to 2D Mobile Game Development','Albert Flores','44.99','https://localhost:7198\images\course-ultimate-guide.svg','false','2.7','98','12.99','100');
+insert into Courses values('Learn JMETER from Scratch on Live Apps-Performance Testing','Jenny Wilson','114.50','https://localhost:7198\images\course-jmetter.svg','false','3.1','92','','160');
