dbe32688f1a04623cd2519f3c81f1ff498db2ead
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-03-28 19:15:32 +0100

    Api key set

---
+++ SilliconWebApi/Filters/UseApiKeyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SilliconWebApi.Filters
+{
+    [AttributeUsage(validOn:AttributeTargets.Class | AttributeTargets.Method)]
+    public class UseApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //hämtar in appsettings.json filen
+            var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = config.GetValue<string>("ApiKey"); // hämtar upp nyckeln
+
+            if(!context.HttpContext.Request.Query.TryGetValue("key", out var providedKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if (!apiKey!.Equals(providedKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
