c5c842a1a544515f35c5d5c24280773521243683
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-04-04 13:08:01 +0200

    added try catch and changed lifetime of the token to 15 min

--- SilliconWebApi/Controllers/AuthController.cs
+++ SilliconWebApi/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using SilliconWebApi.Filters;
+using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
@@ -15,20 +16,25 @@ namespace SilliconWebApi.Controllers
         [Route("token")]
         public IActionResult GetToken()
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]!);
-            var tokenDescripter = new SecurityTokenDescriptor
+            try
             {
-                Expires = DateTime.UtcNow.AddMinutes(30),
-                Issuer = _configuration["Jwt:Issuer"],
-                Audience = _configuration["Jwt:Audience"],
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]!);
+                var tokenDescripter = new SecurityTokenDescriptor
+                {
+                    Expires = DateTime.UtcNow.AddMinutes(15),
+                    Issuer = _configuration["Jwt:Issuer"],
+                    Audience = _configuration["Jwt:Audience"],
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
 
-            var token = tokenHandler.CreateToken(tokenDescripter);
-            var tokenString = tokenHandler.WriteToken(token);
+                var token = tokenHandler.CreateToken(tokenDescripter);
+                var tokenString = tokenHandler.WriteToken(token);
 
-            return Ok(tokenString);
+                return Ok(tokenString);
+            }
+            catch(Exception e) { Debug.WriteLine("Error: " + e.Message); }
+           return Unauthorized();
         }
     }
 }
